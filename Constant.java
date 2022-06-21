8b25b858df9b06b2665bb5ca53ee9ea5ad646433
Alamgir <mdalamgir@gmail.com>
2022-06-21 13:31:29 +0600

    Code style updated

---
+++ Constant.java
@@ -0,0 +1,16 @@
+public class Constant {
+    public static String StudentList ="Students.txt";
+    public static String ShowAll = "a";
+    public static String Loading = "Loading data ...";
+    public static String load = "Data Loaded.";
+    public static String Usage = "Usage: a|r|c|+Word|?Word";
+    public static String StudentEntryDeliminetor = ",";
+    public static String Rew = "r";
+    public static String Add = "+";
+    public static String Date = "dd/mm/yyyy-hh:mm:ss a";
+    public static String Update = "\nList last updated on ";
+    public static String Asking = "?";
+    public static String Found = "We found it!";
+    public static String Count = "c";
+    public static String Founds = " word(s) found ";
+}
